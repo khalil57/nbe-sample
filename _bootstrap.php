659f9acf12040d7e2c3b6b509f12b2adbee952ee
khalil57 <66666716+khalil57@users.noreply.github.com>
2020-06-22 20:00:06 +0200

    Update _bootstrap.php

--- _bootstrap.php
+++ _bootstrap.php
@@ -56,7 +56,7 @@ if (intval($apiVersion) < 39) {
 }
 
 // build api endpoint url
-$gatewayUrl = "https://test-nbe.gateway.mastercard.com/api/rest/version/56/merchant/testsdktest";
+$gatewayUrl = "https://nbe.gateway.mastercard.com/api/rest/version/56/merchant/testnbetest";
 
 // parse query string
 $query = array();
