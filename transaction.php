d4e5ac1c08f7ff97a5e9cd17b62e14043283033e
khalil57 <66666716+khalil57@users.noreply.github.com>
2020-06-23 23:14:13 +0200

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -23,8 +23,7 @@ if (intercept('PUT')) {
     // build path
     $orderId = requiredQueryParam('order');
     $txnId = requiredQueryParam('transaction');
-    $threeDSId = requiredQueryParam('3DSecureId');
-    $path = '/order/' . $orderId . '/transaction/' . $txnId . '/3DSecureId/' . $threeDSId;
+    $path = '/order/' . $orderId . '/transaction/' . $txnId;
 
     proxyCall($path);
 }
